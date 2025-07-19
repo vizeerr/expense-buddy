1a6bbe5db01df9e0166c335a774f0302cad7a03c
Vivek Sagar <vivek54540@gmail.com>
2025-07-19 21:07:58 +0530

    UI CHANGE V2.2

--- components/dashboard/GroupLists.jsx
+++ components/dashboard/GroupLists.jsx
@@ -23,33 +23,19 @@ import { Users } from 'lucide-react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchGroups } from '@/store/slices/group/groupSlice'
+import { useRouter } from 'next/navigation'
 
 const GroupLists = () => {
   const dispatch = useDispatch()
+    const router = useRouter()
   const { list: groups, loading } = useSelector((state) => state.groups)
 
   useEffect(() => {
     dispatch(fetchGroups())
   }, [dispatch])
 
-  if (loading) {
-    return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {[...Array(3)].map((_, i) => (
-          <Skeleton key={i} className="h-40 w-full rounded-lg" />
-        ))}
-      </div>
-    )
-  }
-
-  if (!groups || groups.length === 0) {
-    return (
-      <div className="text-muted-foreground text-center py-8">
-        You are not part of any groups yet.
-      </div>
-    )
-  }
-
+ 
+ 
   return (
     <div className="border p-6 xl:rounded-2xl rounded-2xl bg-transparent drop-shadow-2xl drop-shadow-orange-900 w-full">
         <div className="flex gap-2 items-center">
@@ -57,12 +43,21 @@ const GroupLists = () => {
           <p className="text-xl font-bold">All Groups</p>
         </div>
         <hr className='mt-5'/>
+        {loading && (!groups || groups.length === 0) && <div className="text-muted-foreground text-center py-8">
+        You are not part of any groups yet.
+      </div>}
+        {loading && <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+        {[...Array(3)].map((_, i) => (
+          <Skeleton key={i} className="h-20 w-full rounded-lg" />
+        ))}
+      </div>}
         <Carousel className="mt-5 sm:w-[90%] w-[80%] mx-auto">
         <CarouselContent>
           {groups.map((group, index) => (
             <CarouselItem
               key={index}
               className=" md:basis-1/2 lg:basis-1/3 2xl:basis-1/4"
+              onClick={() => router.push(`/dashboard/groups/${group._id}`)}
             >
               <Card className="hover:border-primary hover:shadow-md transition-all duration-200 h-full bg-transparent">
                 <CardHeader className="flex flex-row items-center gap-3">

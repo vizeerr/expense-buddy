e7f0e98e9ae08354354bdba42f008ac785ab4a52
Vivek Sagar <vivek54540@gmail.com>
2025-08-01 19:14:06 +0530

    PWA

--- components/main/TopNavbar.jsx
+++ components/main/TopNavbar.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 import { useSelector } from 'react-redux'
-
+import {icon} from '@/public/192.png'
+import Image from 'next/image'
 const TopNavbar = () => {
   const user = useSelector((state) => state.auth.user) 
   return (
     <div className='py-3 px-4 flex justify-between items-center fixed top-0 w-full z-3 bg-[#0b000011] backdrop-blur-lg'>
       <div>
-
-        <Link href="/" className='text-xl font-semibold'>Expense Buddy</Link>
+        <Link href="/" className='text-xl font-semibold'>
+          <Image src="icon" width={20} height={20} alt='Logo'/>
+          Expense Buddy
+        </Link>
       </div>
       <div className='flex items-center gap-5'>
         
